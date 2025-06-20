40473b9b68b4a58d7173ddbf8eb4ff5874320088
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 01:13:53 +0000

    [automatic commit] 2025-06-20 01:13:53 UTC 1.147.14.165

--- part1/q5.sql
+++ part1/q5.sql
@@ -13,4 +13,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
   ((SELECT user_id FROM Users WHERE Users.username = 'alice123'), 'Juno', 'medium');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-  ()
+  ((SELECT  FROM Users WHERE Users.username = 'alice123'), 'Max', 'medium'),
