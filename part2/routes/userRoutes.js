19861be7d64b7519540916479a1321e4ce912803
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 10:43:51 +0000

    [automatic commit] 2025-06-20 10:43:51 UTC 1.147.14.165

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -32,10 +32,10 @@ router.get('/dashboard', (req, res) => {
   if (!req.session.user) {
     return res.status(401).sendFile('/public/index.html', { root: '.' });
   }
-  if (req.session.user.role === "owner") {
+  else if (req.session.user.role === "owner") {
     return res.status(200).sendFile('/public/owner-dashboard.html', { root: '.' });
   }
-  if
+  
 });
 
 router.get('/me', (req, res) => {
