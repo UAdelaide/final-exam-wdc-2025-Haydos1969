e32bc49ae97eb9fe7be75153ab4138fa746bb302
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 09:38:10 +0000

    [automatic commit] 2025-06-20 09:38:10 UTC 1.147.14.165

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -53,7 +53,8 @@ router.post('/login', async (req, res) => {
 
     req.session.user = {
       id: rows[0].user_id,
-      
+      username: rows[0].username,
+      role: rows[0].username
     }
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
