31ed90ac3d119087a8a553e13de10b73f7128540
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 02:20:42 +0000

    [automatic commit] 2025-06-20 02:20:42 UTC 1.147.14.165

--- starthere/app.js
+++ starthere/app.js
@@ -76,7 +76,7 @@ app.get('/api/dogs', async (req, res) => {
   } catch (err) {
     res.status(500).json({err: 'failed to fetch users'});
   }
- })
+ });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
