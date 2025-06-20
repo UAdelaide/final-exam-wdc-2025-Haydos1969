6a8fc5e1828d135f09bcbef8dfd18a0e1764db72
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 01:01:23 +0000

    [automatic commit] 2025-06-20 01:01:22 UTC 1.147.14.165

--- part1/q5.sql
+++ part1/q5.sql
@@ -4,3 +4,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
   ('carol123', 'carol@example.com', 'hashed789', 'owner'),
   ('haydos1969', 'hayden.abrahams360@gmail.com', 'hashed321', 'walker'),
   ('personName', 'person@example.com', 'hashed738', 'owner');
+
