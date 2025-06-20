4a01e400150ec639752bda87a561b2707576e309
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 10:56:26 +0000

    [automatic commit] 2025-06-20 10:56:26 UTC 1.147.14.165

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -69,6 +69,7 @@ router.post('/login', async (req, res) => {
       role: rows[0].role
     };
 
+    // Returns the 
     res.json({ message: 'Login successful', user: rows[0] });
 
   } catch (error) {
