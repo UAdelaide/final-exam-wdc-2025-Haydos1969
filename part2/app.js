35af608908cb4832766825e467dc1f1cc1d2fe89
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 10:40:27 +0000

    [automatic commit] 2025-06-20 10:40:26 UTC 1.147.14.165

--- part2/app.js
+++ part2/app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(session({
   secret: 'secret-key',
   resave: false,
-  saveUninitialized
+  saveUninitialized: false,,
   cookie: {
     secure: false,
     maxAge: 1000*60*60
