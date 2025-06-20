bb62d40b7a84db7a8de89a54bed4a51bd7b36587
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 03:29:15 +0000

    [automatic commit] 2025-06-20 03:29:15 UTC 1.147.14.165

--- starthere/app.js
+++ starthere/app.js
@@ -100,7 +100,8 @@ app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [summary] = await db.execute(`SELECT u.username AS walker_username,
                                       (SELECT COUNT(*) FROM WalkRatings r WHERE r.walker_id = u.user_id) AS total_ratings,
-                                      (SELECT AVG(rating) FROM WalkRatings r WHERE r.walker_id = u.user_id) AS average_rating
+                                      (SELECT AVG(rating) FROM WalkRatings r WHERE r.walker_id = u.user_id) AS average_rating,
+                                      
                                       FROM Users AS u
                                       WHERE u.role = 'walker';
                                       `);
