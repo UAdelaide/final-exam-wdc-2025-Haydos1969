aec80d31964115ac7e177ffaad604b2575cd1325
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 01:18:50 +0000

    [automatic commit] 2025-06-20 01:18:50 UTC 1.147.14.165

--- part1/q5.sql
+++ part1/q5.sql
@@ -16,5 +16,5 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
   ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
   ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
   ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Spud'), '2025-06-10 14:00:00', 100, 'Mt Everest', 'cancelled'),
-  ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', open),
+  ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Spotty'), '2025-06-08 08:00:00', 30, 'Parklands', open),
   ((SELECT dog_id FROM Dogs WHERE Dogs.name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', open);
