c40af09a07bceb82941f3b491e319301e3d6aea4
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 03:08:09 +0000

    [automatic commit] 2025-06-20 03:08:09 UTC 1.147.14.165

--- starthere/app.js
+++ starthere/app.js
@@ -99,7 +99,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/dogs', async (req, res) => {
   try {
     const [summary] = await db.execute(`SELECT u.username AS walker_username,
-                                      (SELECT COUNT(*) FROM WalkRatings r WHERE user_id = r.user_id),
+                                      (SELECT COUNT(*) FROM WalkRatings r WHERE r.walker_id = r.user_id),
                                       `);
     res.json(summary);
   } catch (err) {
