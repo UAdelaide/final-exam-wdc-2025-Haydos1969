d8fdf92ebf88577f6a4e665f37a674a32bc63478
Haydos1969 <hayden.abrahams360@gmail.com>
2025-06-20 02:36:50 +0000

    [automatic commit] 2025-06-20 02:36:50 UTC 1.147.14.165

--- starthere/app.js
+++ starthere/app.js
@@ -73,7 +73,7 @@ app.get('/', async (req, res) => {
 
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [dogs] = await db.execute(`SELECT Dogs.name AS dog_name, Dog.size, Users.username AS owner_username
+    const [dogs] = await db.execute(`SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
                                     FROM Dogs
                                     JOIN Users ON Dogs.owner_id = Users.user_id;`);
     res.json(dogs);
